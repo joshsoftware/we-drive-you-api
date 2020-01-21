6907aba62466953d04e1e25a928a58388f36a088
“Durgesh00” <durgesworep589@gmail.com>
2020-01-21 15:16:07 +0530

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  api_version(module: "V1", header: {name: "Accept", value: "application/vnd.dds-api.com; version=1"}) do
+  end
+end
