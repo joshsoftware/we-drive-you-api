ac427731faacec3efeb96eb71d30eccaef7680c6
“Durgesh00” <durgesworep589@gmail.com>
2020-01-23 17:40:45 +0530

    validation for organisation model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,7 +2,8 @@
 
 class Organization < ApplicationRecord
   after_create :create_tenant
-
+  validates :name,:slug presence: true
+  validates :slug uniqueness: true length: { maximum: 10 }
   def tenant_name
     slug.to_s
   end
