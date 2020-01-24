19862b3b356beb701473472f264ad2b7e9ba7363
“Durgesh00” <durgesworep589@gmail.com>
2020-01-24 12:08:17 +0530

    organization model

--- db/migrate/20200124061747_add_columns_to_organization.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToOrganization < ActiveRecord::Migration[5.2]
-  def change
-    add_column :organizations, :email, :string
-    add_column :organizations, :password, :string
-    add_column :organizations, :contact_no, :string
-  end
-end
