d0012fe2ad91a90a52ae1597894b52b0f0fadc56
“Durgesh00” <durgesworep589@gmail.com>
2020-02-03 15:31:30 +0530

    Cab Model Added

--- app/models/cab.rb
+++ app/models/cab.rb
@@ -2,7 +2,7 @@
 
 class Cab < ApplicationRecord
   has_many :rides
-  validates :vehicle_number, :cab_capacity, presence: true
-  validates :vehicle_number, length: {in: 10..10}
-  validates :cab_capacity, numericality: {only_integer: true}
+  validates :vehicle_no, :capacity, presence: true
+  validates :vehicle_no, length: {in: 10..10}
+  validates :capacity, numericality: {only_integer: true}
 end
--- db/migrate/20200203095929_create_cabs.rb
+++ db/migrate/20200203095929_create_cabs.rb
@@ -1,10 +1,9 @@
 class CreateCabs < ActiveRecord::Migration[5.2]
   def change
     create_table :cabs do |t|
-      t.integer :cab_capacity   
-      t.string :Vehicle_number
-      t.integer :cab_id
-      t.integer :driver_id
+      t.integer :capacity
+      t.string :vehicle_no
+
       t.timestamps
     end
   end
