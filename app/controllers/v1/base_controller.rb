8b562118308b593cd24f3690b241a9139032bbc4
akanshak984 <akanshak984@gmail.com>
2020-02-13 12:02:27 +0530

    status code added

--- app/controllers/v1/base_controller.rb
+++ app/controllers/v1/base_controller.rb
@@ -2,7 +2,7 @@
 
 module V1
   class BaseController < ApplicationController
-    def render_json(message:, data: {}, status_code:)
+    def render_json(message:, data: {}, status_code: :ok)
       render json: {message: message, data: data, status: status_code}
     end
   end
